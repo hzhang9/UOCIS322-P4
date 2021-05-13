d584b9aed1d070fb6ee092cbe547e15e3cc26ce1
Haoran Zhang <hzhang9@testium.cs.uoregon.edu>
2021-05-13 03:39:18 -0700

    Complete acp_times.py and pass test on test_open_close_time.py

--- brevets/acp_times.py
+++ brevets/acp_times.py
@@ -42,7 +42,7 @@ def open_time(control_dist_km, brevet_dist_km, brevet_start_time):
     elif 600<control_dist_km<=1000:
         time= 200/34 + 200/32 + 200/30+ (control_dist_km-600)/28
     hrs=time//1
-    mins=(time-hrs)*60
+    mins=round((time-hrs)*60)
     return brevet_start_time.shift(hours=hrs,minutes=mins)
 
 
@@ -66,9 +66,21 @@ def close_time(control_dist_km, brevet_dist_km, brevet_start_time):
     elif control_dist_km==0:
         time=1
     elif 0<control_dist_km<=600:
-        time=control_dist_km/15
+        if control_dist_km==200:
+            time=13.5
+        elif control_dist_km==300:
+            time=20
+        elif control_dist_km==400:
+            time=27
+        elif control_dist_km==600:
+            time=40
+        else:
+            time=control_dist_km/15
     elif 600<control_dist_km<=1000:
-        time=600/15+(control_dist_km-600)/11.428
+        if control_dist_km==1000:
+            time=75
+        else:
+            time=600/15+(control_dist_km-600)/11.428
     hrs=time//1
-    mins=(time-hrs)*60
+    mins=round((time-hrs)*60)
     return brevet_start_time.shift(hours=hrs,minutes=mins)
--- brevets/tests/test_open_close_time.py
+++ brevets/tests/test_open_close_time.py
@@ -18,7 +18,6 @@ def test_cd_exact_0():
     assert acp_times.open_time(0,200,time)==time
     assert acp_times.close_time(0,200,time)==time.shift(hours=1)
 
-
 def test_cd_over_bd_twenty_percent():
     time=arrow.now()
     assert acp_times.open_time(250,200,time)=="the control point is over 20% longer than the theoretical distance"
@@ -28,21 +27,29 @@ def test_cd_0_200():
     time=arrow.now()
     assert acp_times.open_time(112,200,time)==time.shift(hours=3,minutes=18)
     assert acp_times.close_time(112,200,time)==time.shift(hours=7,minutes=28)
+    assert acp_times.open_time(200,200,time)==time.shift(hours=5,minutes=53)
+    assert acp_times.close_time(200,200,time)==time.shift(hours=13,minutes=30)
 
 
 def test_cd_200_400():
     time=arrow.now()
     assert acp_times.open_time(315,400,time)==time.shift(hours=9,minutes=29)
     assert acp_times.close_time(315,400,time)==time.shift(hours=21)
-
+    assert acp_times.open_time(300,400,time)==time.shift(hours=9)
+    assert acp_times.close_time(300,400,time)==time.shift(hours=20)
+    assert acp_times.open_time(400,400,time)==time.shift(hours=12,minutes=8)
+    assert acp_times.close_time(400,400,time)==time.shift(days=1,hours=3)
 
 def test_cd_400_600():
     time=arrow.now()
+    assert acp_times.open_time(469,600,time)==time.shift(hours=14,minutes=26)
+    assert acp_times.close_time(469,600,time)==time.shift(days=1,hours=7,minutes=16)
     assert acp_times.open_time(600,600,time)==time.shift(hours=18,minutes=48)
     assert acp_times.close_time(600,600,time)==time.shift(days=1,hours=16)
-
 
 def test_cd_600_1000():
     time=arrow.now()
     assert acp_times.open_time(789,1000,time)==time.shift(days=1,hours=1,minutes=33)
     assert acp_times.close_time(789,1000,time)==time.shift(days=2,hours=8,minutes=32)
+    assert acp_times.open_time(1000,1000,time)==time.shift(days=1,hours=9,minutes=5)
+    assert acp_times.close_time(1000,1000,time)==time.shift(days=3,hours=3)
